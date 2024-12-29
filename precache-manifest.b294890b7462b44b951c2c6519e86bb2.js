b212c52dae3f3190b11979f8903ac75cb0f3176c
Mohammed Furquan <mohammed.saleem@tectoro.com>
2024-12-29 19:49:57 +0530

    Portfolio update

---
+++ precache-manifest.b294890b7462b44b951c2c6519e86bb2.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9147864a1f756492bd556f463d054f61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7a02e10558c2e0b42c9",
+    "url": "/static/css/main.22718620.chunk.css"
+  },
+  {
+    "revision": "da173b22898032980283",
+    "url": "/static/js/2.6a5fe190.chunk.js"
+  },
+  {
+    "revision": "e7a02e10558c2e0b42c9",
+    "url": "/static/js/main.2c649bc7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "411385cebecca5857d894e0a84f8729e",
+    "url": "/static/media/Jupyter.411385ce.png"
+  },
+  {
+    "revision": "2e8040ec910564a3b481759e17887b9c",
+    "url": "/static/media/Matplotlib.2e8040ec.png"
+  },
+  {
+    "revision": "682b59a561d6f9a9243b981e3395c95a",
+    "url": "/static/media/PyTorch.682b59a5.png"
+  },
+  {
+    "revision": "b6e5ce3d3dacdd8d2f39a5a80d90d986",
+    "url": "/static/media/Tensorflow.b6e5ce3d.png"
+  },
+  {
+    "revision": "74073107cbd12246111053ce2eb9b2c3",
+    "url": "/static/media/adminpanel.74073107.png"
+  },
+  {
+    "revision": "88ed160d0224e293c6a4c4ca1a76c426",
+    "url": "/static/media/andriodstudio.88ed160d.png"
+  },
+  {
+    "revision": "38eea5799e9c9dae718e84c13d79f909",
+    "url": "/static/media/bootstrap.38eea579.png"
+  },
+  {
+    "revision": "9ae97fae7a8fa3a10d22c515c9dc6918",
+    "url": "/static/media/careme.9ae97fae.png"
+  },
+  {
+    "revision": "d449905b12144c76f56ae63a77db3fa6",
+    "url": "/static/media/chaitanya.d449905b.png"
+  },
+  {
+    "revision": "8d2b071d570f79473e6e7c094c7730c1",
+    "url": "/static/media/css.8d2b071d.png"
+  },
+  {
+    "revision": "223290496ee776132e65a2b2d2e7bf2e",
+    "url": "/static/media/dotnet.22329049.png"
+  },
+  {
+    "revision": "6d1779b0b00fac0c6eba1a2ae6864566",
+    "url": "/static/media/dotnetcerti.6d1779b0.jpg"
+  },
+  {
+    "revision": "54ce099d9cf879f823c828274d483898",
+    "url": "/static/media/ecommerce.54ce099d.png"
+  },
+  {
+    "revision": "74df69dcc973d7702fe6505b82d03ba3",
+    "url": "/static/media/edyoda.74df69dc.jpg"
+  },
+  {
+    "revision": "60a21276044075902e5d23e33ec96a21",
+    "url": "/static/media/express.60a21276.png"
+  },
+  {
+    "revision": "ebe880ba05add588e0518ad30d6fe1de",
+    "url": "/static/media/figma.ebe880ba.png"
+  },
+  {
+    "revision": "c521e7e67222276a6860665a46813cc6",
+    "url": "/static/media/firebase.c521e7e6.png"
+  },
+  {
+    "revision": "46bb6c28726b6f9f8afec631b188173d",
+    "url": "/static/media/furLogo.46bb6c28.png"
+  },
+  {
+    "revision": "20cae2e20cb59a93378da813b1fa52f0",
+    "url": "/static/media/furquan.20cae2e2.jpeg"
+  },
+  {
+    "revision": "b1647ad01646c9f07ae4f6ae53e06f0c",
+    "url": "/static/media/furquan3.b1647ad0.JPG"
+  },
+  {
+    "revision": "e54e9f14248f4195329e3586f4f7e789",
+    "url": "/static/media/furquan4.e54e9f14.jpg"
+  },
+  {
+    "revision": "7c3933e2dd58a1ba4b7ede83407faaea",
+    "url": "/static/media/git.7c3933e2.png"
+  },
+  {
+    "revision": "3e2a192e5b97066b40df2a614b528635",
+    "url": "/static/media/html.3e2a192e.png"
+  },
+  {
+    "revision": "f3a874a9f1487a21d3cde1d82792343b",
+    "url": "/static/media/internship1.f3a874a9.png"
+  },
+  {
+    "revision": "21a0cecce1032b62e0f9a5764a733b53",
+    "url": "/static/media/internship2.21a0cecc.png"
+  },
+  {
+    "revision": "1bcea44330091682a7583543bb33669f",
+    "url": "/static/media/jquery.1bcea443.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/static/media/js.0cded3a3.png"
+  },
+  {
+    "revision": "acdb07b6b8a43455e5173db5bd3d4599",
+    "url": "/static/media/kau.acdb07b6.jpeg"
+  },
+  {
+    "revision": "0eb5243f4c4d0a32f60b280527378398",
+    "url": "/static/media/lanchain.0eb5243f.png"
+  },
+  {
+    "revision": "20d8652c9fd1853ac11a27da7b915ac1",
+    "url": "/static/media/mongodb.20d8652c.png"
+  },
+  {
+    "revision": "c89c837abfabd927ecb42421b6fcd576",
+    "url": "/static/media/mysql.c89c837a.png"
+  },
+  {
+    "revision": "bf3efff2d50f5a3b2c0bcee8c268fcf2",
+    "url": "/static/media/netlify.bf3efff2.png"
+  },
+  {
+    "revision": "f64d59b04c8a579ea7c0d32f0eac90f9",
+    "url": "/static/media/nims.f64d59b0.png"
+  },
+  {
+    "revision": "24250f52da3f77849b561e6ea2f6e5d4",
+    "url": "/static/media/nn.24250f52.png"
+  },
+  {
+    "revision": "3b12f93bf3d79dd7c4c045f9698912a2",
+    "url": "/static/media/node.3b12f93b.png"
+  },
+  {
+    "revision": "96d441a1348ff026335baab5963b0d09",
+    "url": "/static/media/ou.96d441a1.jpg"
+  },
+  {
+    "revision": "5a87787ae1e897ca57279a0aa15c460d",
+    "url": "/static/media/pycerti.5a87787a.jpg"
+  },
+  {
+    "revision": "f46553a7da22fe5ee12134e14016ebee",
+    "url": "/static/media/quiz.f46553a7.png"
+  },
+  {
+    "revision": "3774aef76685771f440b57c7092894a0",
+    "url": "/static/media/rapidapi.3774aef7.png"
+  },
+  {
+    "revision": "776fd8269417774ef8b29304781e5277",
+    "url": "/static/media/react.776fd826.png"
+  },
+  {
+    "revision": "812403883987b4e7711bf576c5c17c55",
+    "url": "/static/media/reactcerti.81240388.png"
+  },
+  {
+    "revision": "b8c8b66a1945157d100bdb3bbb3a3305",
+    "url": "/static/media/reactnative.b8c8b66a.png"
+  },
+  {
+    "revision": "f0f50003d0e332fe8232c3d685b6b221",
+    "url": "/static/media/scikit.f0f50003.png"
+  },
+  {
+    "revision": "2878e6dfdd0eabe061dc96019cead6dc",
+    "url": "/static/media/sop.2878e6df.png"
+  },
+  {
+    "revision": "a0d936d247ac33ab6169013773842f8f",
+    "url": "/static/media/studio.a0d936d2.png"
+  },
+  {
+    "revision": "f55415a6748a31a27be24511c8e10783",
+    "url": "/static/media/tenderprism.f55415a6.jpg"
+  },
+  {
+    "revision": "414fb748ddc37e14574f4be3f896ea45",
+    "url": "/static/media/todo.414fb748.png"
+  },
+  {
+    "revision": "3d09b4bdb319b017cf86e464ee3becdc",
+    "url": "/static/media/travel.3d09b4bd.png"
+  },
+  {
+    "revision": "c069e6e9e0ae632456adf0a088c40133",
+    "url": "/static/media/vehicle.c069e6e9.jpg"
+  },
+  {
+    "revision": "a78dfa30bc7f1e6e652284bdd386d0d6",
+    "url": "/static/media/vscode.a78dfa30.png"
+  },
+  {
+    "revision": "3ddae036335e5b4864ce5b9b76645eb9",
+    "url": "/static/media/webcerti.3ddae036.png"
+  },
+  {
+    "revision": "429dbd4063b50f689221120cb2f26a19",
+    "url": "/static/media/ybi.429dbd40.jpg"
+  }
+]);
